60ea1c75bc97963e441c1e45500bbf6ce7eb5c1a
Austin Back <tartsofficial@gmail.com>
2023-03-09 08:20:11 -0500

    Create makeFancyString.go

---
+++ Delete Characters to Make Fancy String/makeFancyString.go
@@ -0,0 +1,14 @@
+func makeFancyString(s string) string {
+    if len(s) < 3 { return s }
+    ans, tmp := []byte{}, []byte(s)
+    for i := 2; i < len(s); i++ {
+        b := false
+        c := tmp[i - 2]
+        temp := tmp[i - 2:i + 1]
+        for j := 1; j < 3; j++ {
+            if temp[j] != c { b = true; break }
+        }
+        if b { ans = append(ans, c) }
+    }
+    return string(append(ans, tmp[len(tmp) - 2:]...))
+}
